c60a2fb9454bceb9225d3d79983ce93372b24cc3
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 12:10:22 +0530

    added auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Leaf } from "lucide-react";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <section className="max-w-sm mx-auto my-24 p-4">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex justify-center items-center gap-x-3">
+          <Button className="pointer-events-none" variant="secondary">
+            <Leaf />
+          </Button>
+          <p className="text-xl font-medium text-gray-600 hidden sm:inline-flex">
+            Eco Swachh
+          </p>
+        </div>
+        <p className="text text-neutral-400 font-light">
+          One more step towards the clean India
+        </p>
+      </div>
+      {children}
+    </section>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function RegisterPage() {
+  return (
+    <section className="my-6 w-full">
+      <div>Register</div>
+    </section>
+  );
+}
