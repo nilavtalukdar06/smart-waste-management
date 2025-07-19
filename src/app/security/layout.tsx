e9d07d30f90884ec47b940264a9c887801573e68
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 23:24:42 +0530

    handled uploadthing errors

--- src/app/security/layout.tsx
+++ src/app/security/layout.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Leaf } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import authOptions from "@/lib/auth";
 
 export default async function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <section className="max-w-sm mx-auto my-12 p-4">
       <div className="flex flex-col justify-center items-center gap-4">
--- src/app/security/page.tsx
+++ src/app/security/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import Error from "@/components/shared/error";
+import { UploadDropzone } from "@/components/upload/uploadthing";
+import { useState } from "react";
+
 export default function VerifyAccount() {
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [error, setError] = useState<string>("");
   return (
-    <section>
-      <h2>Verify Account</h2>
+    <section className="my-6 w-full">
+      {error && (
+        <Error error="Failed to upload image, please try again later" />
+      )}
+      <UploadDropzone
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          setImageUrl(res[0].ufsUrl);
+        }}
+        onUploadError={(error) => {
+          setError(error?.message);
+        }}
+      />
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          height={200}
+          width={300}
+          className="object-cover object-center"
+        />
+      )}
     </section>
   );
 }
