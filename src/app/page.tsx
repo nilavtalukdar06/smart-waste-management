c709c4c8038cfaecce494485b7fd9348330757d4
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 10:32:02 +0530

    Add button component and update styles; enhance global CSS variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <section>
+    <section className="p-4">
       <h1>NEXT Js</h1>
+      <Button variant="outline">Click Me</Button>
     </section>
   );
 }
