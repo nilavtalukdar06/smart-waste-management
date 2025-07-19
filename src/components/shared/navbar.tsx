caa02bfcf3c94ab06475e2b359053e49f6fc4269
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 16:55:18 +0530

    completed auth flow

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { ArrowRight, Leaf } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { Skeleton } from "../ui/skeleton";
+import { useSession } from "next-auth/react";
+import LogoutButton from "../auth/logout-button";
 
 export default function Navbar() {
+  const { data: session, status } = useSession();
   return (
     <header className="px-5 py-4 w-full flex justify-between items-center">
       <div className="flex justify-center items-center gap-x-3">
@@ -26,11 +31,17 @@ export default function Navbar() {
             <p className="text-neutral-500">Coins: 50</p>
           </div>
         )}
-        <Link href="/login">
-          <Button>
-            Get Started <ArrowRight className="hidden sm:inline-flex" />
-          </Button>
-        </Link>
+        {status === "loading" ? (
+          <Skeleton className="h-[36px] w-[100px]" />
+        ) : session ? (
+          <LogoutButton />
+        ) : (
+          <Link href="/login">
+            <Button>
+              Get Started <ArrowRight className="hidden sm:inline-flex" />
+            </Button>
+          </Link>
+        )}
       </div>
     </header>
   );
