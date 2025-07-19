19553d4578d42e5af4e96b512c18ce7a8ce10f69
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 16:45:46 +0530

    added protection to auth routes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Leaf } from "lucide-react";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <section className="max-w-sm mx-auto my-12 p-4">
       <div className="flex flex-col justify-center items-center gap-4">
