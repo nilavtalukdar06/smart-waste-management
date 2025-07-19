5737992aace7d2a136ccf0834817577f545633f3
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 22:52:37 +0530

    added uploadthing middleware

---
+++ src/utils/upload/uploadthing.ts
@@ -0,0 +1,24 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/auth";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({
+    image: {
+      maxFileSize: "8MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const session = await getServerSession(authOptions);
+      if (!session) throw new UploadThingError("Unauthorized");
+      return { userId: session.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      return { uploadedBy: metadata.userId, fileUrl: file.ufsUrl };
+    }),
+} satisfies FileRouter;
+export type OurFileRouter = typeof ourFileRouter;
