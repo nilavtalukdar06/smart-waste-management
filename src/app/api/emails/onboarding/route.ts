029d99bf8f6abddb99a965ffe950d146022c5717
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 20:22:21 +0530

    improved email sending api

--- src/app/api/emails/onboarding/route.ts
+++ src/app/api/emails/onboarding/route.ts
@@ -1,17 +1,33 @@
 import { EmailTemplate } from "@/components/email/email-template";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  name: z.string().min(2, { message: "name is too short" }),
+  email: z.string().email({ message: "invalid email" }),
+});
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(request: NextRequest) {
   try {
     const { email, name } = await request.json();
+    if (!email || !name) {
+      return NextResponse.json(
+        { error: "missing required fields" },
+        { status: 400 }
+      );
+    }
+    const parsedData = bodySchema.safeParse({ email, name });
+    if (!parsedData.success) {
+      return NextResponse.json({ error: "invalid data" }, { status: 400 });
+    }
     const { data, error } = await resend.emails.send({
-      from: "EcoSwachh <nilavtalukdar06@gmail.com>",
-      to: [email],
+      from: "EcoSwachh <nilavtalukdar06@imagify.space>",
+      to: [parsedData.data.email],
       subject: "🌱 Welcome to Eco Swachh - Let's Clean Up Together!",
-      react: EmailTemplate({ name: name }),
+      react: EmailTemplate({ name: parsedData.data.name }),
     });
 
     if (error) {
