10dab968fb8d53083142f2c191f665cb36018f0f
Nilav <nilavtalukdar06@gmail.com>
2025-07-19 12:03:34 +0530

    implemented session context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import ProtectedRoutes from "@/components/auth/context/protected-routes";
 
 const nunito = Nunito({
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "EcoSwachh",
-  description: "EcoSwachh is a smart waste management platform empowering citizens to report garbage, verify collections, and earn eco-coins. Backed by AI and aligned with Swachh Bharat Abhiyan, EcoSwachh promotes a cleaner, greener, and smarter India.",
+  description:
+    "EcoSwachh is a smart waste management platform empowering citizens to report garbage, verify collections, and earn eco-coins. Backed by AI and aligned with Swachh Bharat Abhiyan, EcoSwachh promotes a cleaner, greener, and smarter India.",
 };
 
 export default function RootLayout({
@@ -18,10 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${nunito.className} antialiased`}
-      >
-        <main>{children}</main>
+      <body className={`${nunito.className} antialiased`}>
+        <ProtectedRoutes>
+          <main>{children}</main>
+        </ProtectedRoutes>
       </body>
     </html>
   );
