a5e84ed71fb6cff497d3bf8c0516e9801ad0cf80
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 11:42:01 +0530

    added resend success email api route

---
+++ src/app/api/emails/success/route.ts
@@ -0,0 +1,45 @@
+import SuccessEmail from "@/components/email/success-email";
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  name: z.string().min(2, { message: "name is too short" }),
+  email: z.string().email({ message: "invalid email" }),
+});
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, name } = await request.json();
+    if (!email || !name) {
+      return NextResponse.json(
+        { error: "missing required fields" },
+        { status: 400 }
+      );
+    }
+    const parsedData = bodySchema.safeParse({ email, name });
+    if (!parsedData.success) {
+      return NextResponse.json({ error: "invalid data" }, { status: 400 });
+    }
+    const { data, error } = await resend.emails.send({
+      from: "EcoSwachh <nilavtalukdar06@imagify.space>",
+      to: [parsedData.data.email],
+      subject: "Verification Success",
+      react: SuccessEmail({ name: parsedData.data.name }),
+    });
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to send the email" },
+      { status: 500 }
+    );
+  }
+}
