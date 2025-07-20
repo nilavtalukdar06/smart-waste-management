ec570be63fcd7d6b47ec46ccb4f7379d35fd8478
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 11:53:09 +0530

    minor ui updates

--- src/components/email/success-email.tsx
+++ src/components/email/success-email.tsx
@@ -5,7 +5,7 @@ interface ISuccess {
 export default function SuccessEmail({ name }: ISuccess) {
   return (
     <div>
-      <h1 className="text-2xl font-medium text-green-500">Hi , {name}!</h1>✅
+      <h1 className="text-2xl font-medium text-green-500">Hi , {name}!</h1>
       <p className="my-4 text-lg text-neutral-700">
         ✅ Your account has been successfully created! You can now log in and
         start using all the features of our platform. Thank you for registering
