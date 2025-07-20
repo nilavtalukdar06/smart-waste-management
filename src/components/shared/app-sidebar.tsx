a579d31f2a9b0b52d95e2559209c7781466aacc3
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 16:12:56 +0530

    added mock sidebar

--- src/components/shared/app-sidebar.tsx
+++ src/components/shared/app-sidebar.tsx
@@ -1,7 +1,61 @@
+import { BrainCircuit, LayoutDashboard, Medal, Recycle } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Chatbot",
+    url: "/dashboard/chat",
+    icon: BrainCircuit,
+  },
+  {
+    title: "Reports",
+    url: "/dashboard/reports",
+    icon: Recycle,
+  },
+  {
+    title: "Leaderboard",
+    url: "/dashboard/leaderboard",
+    icon: Medal,
+  },
+];
+
 export default function AppSidebar() {
   return (
-    <div>
-      <h3>App Sidebar</h3>
-    </div>
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 }
