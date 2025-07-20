dee8d309cf5fe801535b1cd1d720e2a605cb8bee
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 15:34:00 +0530

    added protection to protected routes

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,6 +1,15 @@
-export default function Layout({
+import authOptions from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <div>{children}</div>
