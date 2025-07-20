255de2ab3008ddf6b0a8362334906877a27c5285
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 13:50:54 +0530

    minor ui updates

--- src/components/shared/error.tsx
+++ src/components/shared/error.tsx
@@ -6,7 +6,7 @@ export default function Error({ error }: { error: string }) {
       className="p-4 mb-4 text-sm w-full text-red-800 rounded-lg bg-red-50 flex justify-start gap-x-4 items-center"
       role="alert"
     >
-      <TriangleAlert className="text-red-500 shrink-0" size={16} />
+      <TriangleAlert className="text-red-500" />
       <p>{error}</p>
     </div>
   );
--- src/components/shared/success.tsx
+++ src/components/shared/success.tsx
@@ -6,7 +6,7 @@ export default function Success({ message }: { message: string }) {
       className="p-4 mb-4 w-full flex justify-start items-center gap-x-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
       role="alert"
     >
-      <span className="font-medium shrink-0">
+      <span className="font-medium">
         <CircleCheckBig className="text-green-800" />
       </span>
       <span>{message}</span>
