ec543b697747c987c894363c12e6cd9218cf1b4e
Nilav <nilavtalukdar06@gmail.com>
2025-07-20 17:00:45 +0530

    completed sidebar

--- src/components/shared/app-sidebar.tsx
+++ src/components/shared/app-sidebar.tsx
@@ -1,15 +1,28 @@
-import { BrainCircuit, LayoutDashboard, Medal, Recycle } from "lucide-react";
-
+"use client";
+import {
+  BrainCircuit,
+  LayoutDashboard,
+  Medal,
+  Recycle,
+  Leaf,
+  HandCoins,
+} from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "../ui/skeleton";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const items = [
   {
@@ -19,36 +32,56 @@ const items = [
   },
   {
     title: "Chatbot",
-    url: "/dashboard/chat",
+    url: "/chat",
     icon: BrainCircuit,
   },
   {
     title: "Reports",
-    url: "/dashboard/reports",
+    url: "/reports",
     icon: Recycle,
   },
   {
     title: "Leaderboard",
-    url: "/dashboard/leaderboard",
+    url: "/leaderboard",
     icon: Medal,
   },
 ];
 
 export default function AppSidebar() {
+  const { data: session, status } = useSession();
+  const pathname = usePathname();
+
   return (
     <Sidebar>
+      <SidebarHeader>
+        <div className="flex flex-col justify-center items-start gap-y-6">
+          <div className="flex justify-center items-center gap-x-3">
+            <Button className="pointer-events-none" variant="secondary">
+              <Leaf />
+            </Button>
+            <p className="text-xl font-medium text-gray-800">Eco Swachh</p>
+          </div>
+          {status === "loading" ? (
+            <Skeleton className="w-full h-4 rounded-lg" />
+          ) : (
+            <p className="text-sm text-neutral-600">{session?.user.email}</p>
+          )}
+        </div>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="gap-y-2">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                  <SidebarMenuButton
+                    asChild
+                    className={`${pathname === item.url ? "bg-green-500 text-white hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white" : ""}`}
+                  >
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -56,6 +89,14 @@ export default function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <Link href="/donate-us" className="w-full">
+          <Button variant="secondary" className="w-full">
+            <HandCoins />
+            Donate Us
+          </Button>
+        </Link>
+      </SidebarFooter>
     </Sidebar>
   );
 }
