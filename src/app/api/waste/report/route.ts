d10bf0a209712d09f0e6ca588a18f0b28493516c
Nilav <nilavtalukdar06@gmail.com>
2025-07-21 12:17:18 +0530

    added api routes for waste reporting

---
+++ src/app/api/waste/report/route.ts
@@ -0,0 +1,63 @@
+import connectToMongoDb from "@/db";
+import authOptions from "@/lib/auth";
+import Waste from "@/models/waste.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  type: z.string().min(2, { message: "waste type is too short" }),
+  items: z.string().min(2, { message: "waste items are too short" }),
+  weight: z.string().min(1, { message: "weight is too short" }),
+  confidenceScore: z.number(),
+  location: z.string().min(2, { message: "location is too short" }),
+  imageUrl: z
+    .string()
+    .min(2, { message: "image url is too short" })
+    .regex(
+      /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$/,
+      { message: "image url is not valid" }
+    ),
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "user is not authenticated" },
+        { status: 401 }
+      );
+    }
+    const body = await request.json();
+    if (
+      !body.type ||
+      !body.items ||
+      !body.weight ||
+      body.confidenceScore === undefined ||
+      !body.location ||
+      !body.imageUrl
+    ) {
+      return NextResponse.json(
+        { error: "missing required fields" },
+        { status: 400 }
+      );
+    }
+    const parsedBody = bodySchema.safeParse(body);
+    if (!parsedBody.success) {
+      return NextResponse.json({ error: parsedBody.error }, { status: 400 });
+    }
+    await connectToMongoDb();
+    await Waste.create({ ...parsedBody.data, reporter: session?.user?.id });
+    return NextResponse.json(
+      { message: "successfully reported waste" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to report waste" },
+      { status: 500 }
+    );
+  }
+}
