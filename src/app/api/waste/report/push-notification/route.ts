c6f895a9e9ebf478e926a756e846263454a3a167
Nilav <nilavtalukdar06@gmail.com>
2025-07-21 22:12:11 +0530

    added api routes for push notifications

---
+++ src/app/api/waste/report/push-notification/route.ts
@@ -0,0 +1,50 @@
+import authOptions from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import Pusher from "pusher";
+import { z } from "zod";
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
+  useTLS: true,
+});
+
+const bodySchema = z.object({
+  location: z.string().min(2, { message: "location is too short" }),
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "user is not authenticated" },
+        { status: 401 }
+      );
+    }
+    const body = await request.json();
+    if (!body.location) {
+      return NextResponse.json(
+        { error: "location is not sent" },
+        { status: 400 }
+      );
+    }
+    const parsedBody = bodySchema.safeParse(body);
+    if (!parsedBody.success) {
+      return NextResponse.json({ error: parsedBody?.error }, { status: 400 });
+    }
+    await pusher.trigger("waste-channel", "waste-reported", {
+      message: `Waste reported by ${session.user.name || "someone"} at ${parsedBody.data.location}`,
+    });
+    return NextResponse.json({ message: "sent notification" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to push notifications" },
+      { status: 500 }
+    );
+  }
+}
