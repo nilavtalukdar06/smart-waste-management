b102cc461a7798cf96b89f75c6f447abee764351
Nilav <nilavtalukdar06@gmail.com>
2025-07-21 13:41:21 +0530

    added initial dashboard layout

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,7 +1,34 @@
-export default function Dashboard() {
+import authOptions from "@/lib/auth";
+import { TriangleAlert } from "lucide-react";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
+  const nameArray = session?.user.name.split(" ");
+  const firstName = Array.from(nameArray[0]).join("");
+
   return (
-    <div>
-      <p>Dashboard</p>
-    </div>
+    <section className="p-4">
+      <h2 className="text-2xl sm:text-3xl font-medium text-neutral-600">
+        Welcome {firstName}😎
+      </h2>
+      {session.user.isVerified && (
+        <div className="my-4 p-4 flex justify-start text-sm items-center bg-red-50 rounded-lg gap-x-6 font-light">
+          <TriangleAlert className="text-red-500" />
+          <div className="text-red-500">
+            You have not verified your account yet, please click on this link to{" "}
+            <Link href="/security" className="underline text-red-700">
+              verify.
+            </Link>{" "}
+            If you failed to do so, your account will get deleted after 7 days.
+          </div>
+        </div>
+      )}
+    </section>
   );
 }
