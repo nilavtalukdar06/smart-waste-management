59bafaae52c8a65e8a8a6ebeee7575ff28934545
Nilav <nilavtalukdar06@gmail.com>
2025-07-21 10:00:23 +0530

    added db schema

---
+++ src/models/waste.model.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { IWaste } from "../../types/schema";
+
+const wasteSchema = new mongoose.Schema<IWaste>(
+  {
+    type: {
+      type: String,
+      required: [true, "type of waste is required"],
+    },
+    items: {
+      type: String,
+      required: [true, "waste items are required"],
+    },
+    weight: {
+      type: String,
+      required: [true, "weight is required"],
+    },
+    confidenceScore: {
+      type: Number,
+    },
+    location: {
+      type: String,
+      required: [true, "location is required"],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["not-collected", "pending", "collected"],
+      default: "not-collected",
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      match: [
+        /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$/,
+        "image url is not valid",
+      ],
+    },
+    reporter: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true],
+    },
+  },
+  { timestamps: true }
+);
+
+const Waste =
+  mongoose.models?.Waste || mongoose.model<IWaste>("Waste", wasteSchema);
+
+export default Waste;
--- types/schema.d.ts
+++ types/schema.d.ts
@@ -11,3 +11,17 @@ interface IUser {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+interface IWaste {
+  _id?: mongoose.Types.ObjectId;
+  type: string;
+  items: string;
+  weight: string;
+  confidenceScore: number;
+  status: string;
+  location: string;
+  imageUrl: string;
+  reporter: mongoose.Schema.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
