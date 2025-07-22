17c949e72e1a2cf084fb40e995dd10dfcaa1bce1
Nilav <nilavtalukdar06@gmail.com>
2025-07-22 20:20:23 +0530

    added card items

---
+++ src/lib/constants.tsx
@@ -0,0 +1,16 @@
+import { Heart, Leaf } from "lucide-react";
+
+export const cardItems = [
+  {
+    icon: Leaf,
+    title: "Environmental Impact",
+    description:
+      "Your contribution helps clean our environment and promotes sustainable waste management practices in communities.",
+  },
+  {
+    icon: Heart,
+    title: "Community Support",
+    description:
+      "You're supporting local communities and waste management workers who keep our neighborhoods clean and healthy.",
+  },
+];
