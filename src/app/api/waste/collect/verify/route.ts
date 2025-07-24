acb30083ab9e5e2449aec9852c8125f922eaa7b4
Nilav <nilavtalukdar06@gmail.com>
2025-07-24 15:15:13 +0530

    completed the api

--- src/app/api/waste/collect/verify/route.ts
+++ src/app/api/waste/collect/verify/route.ts
@@ -1,4 +1,5 @@
 import authOptions from "@/lib/auth";
+import { inngest } from "@/lib/inngest";
 import collect from "@/utils/gcp/collect-waste";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -19,6 +20,7 @@ const requestSchema = z.object({
       /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$/,
       { message: "image url is not valid" }
     ),
+  reportId: z.string().min(2),
 });
 
 export async function POST(request: NextRequest) {
@@ -38,12 +40,25 @@ export async function POST(request: NextRequest) {
         { status: 400 }
       );
     }
-    const result = await collect(body.reportedImageUrl, body.collectedImageUrl);
+    const result = await collect(
+      parsedBody.data.reportedImageUrl,
+      parsedBody.data.collectedImageUrl
+    );
     if (!result) {
       return NextResponse.json({ error: "failed to verify" }, { status: 404 });
     }
     const response = result.replace(/```json|```/g, "").trim();
     const parsedResponse = JSON.parse(response);
+    if (parsedResponse?.isValid) {
+      await inngest.send({
+        name: "waste/verified",
+        data: {
+          userId: session.user.id,
+          name: session.user.name,
+          reportId: parsedBody.data.reportId,
+        },
+      });
+    }
     return NextResponse.json(parsedResponse, { status: 200 });
   } catch (error) {
     console.error(error);
