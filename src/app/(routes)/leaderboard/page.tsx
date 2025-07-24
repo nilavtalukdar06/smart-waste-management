30d0bd687f661eeced58ea257ef770911ec40726
Nilav <nilavtalukdar06@gmail.com>
2025-07-24 22:37:35 +0530

    added search bar

--- src/app/(routes)/leaderboard/page.tsx
+++ src/app/(routes)/leaderboard/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { useState } from "react";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
 export default function Leaderboard() {
+  const [searchTerm, setSearchTerm] = useState<string>("");
   return (
     <div className="p-4">
-      <div>Leaderboard</div>
+      <h2 className="text-3xl font-medium text-neutral-600">Leaderboard 👑</h2>
+      <div className="max-w-md mr-auto my-4">
+        <div className="flex justify-center items-center px-4 rounded-lg border shadow-xs">
+          <Search size={16} className="text-neutral-600" />
+          <Input
+            placeholder="Search top performers by name or email"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border-0 focus:border-0 focus:outline-none focus-visible:ring-0"
+          />
+        </div>
+      </div>
     </div>
   );
 }
