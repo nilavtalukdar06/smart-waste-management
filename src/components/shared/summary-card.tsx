041ee8efab8674c1eb5e9edbf72660cfb9ac15e4
Nilav <nilavtalukdar06@gmail.com>
2025-07-25 20:49:12 +0530

    created ui cards

--- src/components/shared/summary-card.tsx
+++ src/components/shared/summary-card.tsx
@@ -1,7 +1,20 @@
+import { Recycle } from "lucide-react";
+import { Button } from "../ui/button";
+
 export default function SummaryCard() {
   return (
-    <div className="my-4">
-      <div>Summary Card</div>
+    <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-4">
+      <div className="w-full p-4 border rounded-lg bg-neutral-100">
+        <div className="flex flex-col justify-center items-start gap-y-3">
+          <Button className="pointer-events-none" variant="secondary" size="sm">
+            <Recycle />
+          </Button>
+          <h3 className="text-xl font-medium text-neutral-800">
+            Total Reports Submitted
+          </h3>
+        </div>
+        <p className="text-3xl font-semibold text-neutral-700 mt-2">17</p>
+      </div>
     </div>
   );
 }
