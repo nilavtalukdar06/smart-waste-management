99f3d09689e32b2fbec4b32fe12a643a7722bf12
Nilav <nilavtalukdar06@gmail.com>
2025-07-25 15:24:15 +0530

    added a mock dialog for viewing disposal methods

---
+++ src/components/dialog/disposal-method.tsx
@@ -0,0 +1,41 @@
+"use client";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { AlertCircle, Recycle } from "lucide-react";
+
+export default function DisposalMethod() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="sm" variant="destructive">
+          Disposal Method <Recycle />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button className="w-full" variant="destructive">
+              Close <AlertCircle />
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
