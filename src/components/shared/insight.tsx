484030609c3c6f5e8170c61f347895a6b60a1df1
Nilav <nilavtalukdar06@gmail.com>
2025-07-25 22:35:21 +0530

    added ai insight dynamically

--- src/components/shared/insight.tsx
+++ src/components/shared/insight.tsx
@@ -1,6 +1,8 @@
 "use client";
 import fetchInsight from "@/utils/api/insight";
 import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import Error from "./error";
 
 export default function Insight() {
   const { data, isLoading, isError } = useQuery({
@@ -8,23 +10,35 @@ export default function Insight() {
     queryFn: fetchInsight,
   });
 
-  return (
-    <article className="p-4 md:p-6 border rounded-lg">
-      <h1 className="text-2xl md:text-3xl font-medium text-neutral-600">
-        ✨ AI Insights
-      </h1>
-      <p className="my-4 text-sm font-light md:text-base text-neutral-500">
-        Over the past month, user engagement has increased significantly, with a
-        34% rise in daily waste reports, primarily driven by plastic and paper
-        submissions. The leaderboard saw intense competition, with top users
-        submitting over 150 verified reports. Collection accuracy improved by
-        22%, while average response time dropped to under 10 hours. Plastic
-        waste remains the most reported type, though biodegradable waste
-        collections are growing steadily. Peak reporting occurs at 11 AM and 7
-        PM. Reward redemptions rose by 40%, especially among users aged 18-25.
-        These insights highlight growing environmental awareness and the
-        platform&apos;s impact on sustainable waste practices.
-      </p>
-    </article>
-  );
+  if (isLoading) {
+    return (
+      <div className="flex justify-start items-center gap-x-4">
+        <Loader className="text-green-500 animate-spin" size={24} />
+        <p className="text-lg font-medium text-neutral-600">
+          Fetching AI Insight...
+        </p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="max-w-md mr-auto">
+        <Error error="Error fetching AI insight" />
+      </div>
+    );
+  }
+
+  if (data) {
+    return (
+      <article className="p-4 md:p-6 border rounded-lg">
+        <h1 className="text-2xl md:text-3xl font-medium text-neutral-600">
+          ✨ AI Insights
+        </h1>
+        <p className="my-4 text-sm font-light md:text-base text-neutral-500">
+          {data}
+        </p>
+      </article>
+    );
+  }
 }
