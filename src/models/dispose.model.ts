280795829d485739f7d5a5b82643fe7bf8f4ee0c
Nilav <nilavtalukdar06@gmail.com>
2025-07-25 14:14:14 +0530

    added database schema for disposing waste

---
+++ src/models/dispose.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { IDisposal } from "../../types/schema";
+
+const disposalSchema = new mongoose.Schema<IDisposal>(
+  {
+    report: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Waste",
+    },
+    isValid: {
+      type: Boolean,
+      required: true,
+    },
+    disposalMethod: {
+      type: String,
+      required: true,
+    },
+    warning: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Dispose =
+  mongoose.models?.Dispose ||
+  mongoose.model<IDisposal>("Dispose", disposalSchema);
+
+export default Dispose;
