e257e835432bf5fb959096f45d725f290732ef37
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 20:03:41 +0530

    validated the request body

--- src/app/api/emails/subscribe/route.ts
+++ src/app/api/emails/subscribe/route.ts
@@ -1,7 +1,26 @@
 import { NextResponse, NextRequest } from "next/server";
+import { z } from "zod";
+
+const requestSchema = z.object({
+  email: z.string().email({ message: "email is not valid" }),
+});
 
 export async function POST(request: NextRequest) {
   try {
+    const body = await request.json();
+    if (!body.email) {
+      return NextResponse.json(
+        { error: "email is not present in the request body" },
+        { status: 400 }
+      );
+    }
+    const parsedBody = requestSchema.safeParse(body);
+    if (!parsedBody.success) {
+      return NextResponse.json(
+        { error: parsedBody?.error.format() },
+        { status: 400 }
+      );
+    }
     return NextResponse.json({ success: true }, { status: 201 });
   } catch (error) {
     console.error(error);
