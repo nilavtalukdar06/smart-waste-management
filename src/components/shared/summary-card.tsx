afe763cfae0604af467079df04cf8746b02253a6
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 12:57:14 +0530

    added loading ui to summary card component

--- src/components/shared/summary-card.tsx
+++ src/components/shared/summary-card.tsx
@@ -22,75 +22,62 @@ export default function SummaryCard() {
     );
   }
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-start items-center gap-x-4 my-4">
-        <Loader className="animate-spin text-green-500" size={24} />
-        <p className="text-lg font-medium text-neutral-500">
-          Fetching Summary...
+  return (
+    <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-4">
+      {/* card 1 */}
+      <div className="w-full p-4 border rounded-lg bg-sidebar">
+        <div className="flex flex-col justify-center items-start gap-y-3">
+          <Button className="pointer-events-none" variant="secondary" size="sm">
+            <TriangleAlert />
+          </Button>
+          <h3 className="text-xl font-medium text-neutral-800">
+            Total Reports Submitted
+          </h3>
+        </div>
+        <p className="text-3xl font-semibold text-neutral-700 mt-2">
+          {isLoading ? (
+            <Loader className="text-green-500 animate-spin" size={24} />
+          ) : (
+            <span>{data.reports}</span>
+          )}
         </p>
       </div>
-    );
-  }
-
-  if (data) {
-    return (
-      <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-4">
-        {/* card 1 */}
-        <div className="w-full p-4 border rounded-lg bg-sidebar">
-          <div className="flex flex-col justify-center items-start gap-y-3">
-            <Button
-              className="pointer-events-none"
-              variant="secondary"
-              size="sm"
-            >
-              <TriangleAlert />
-            </Button>
-            <h3 className="text-xl font-medium text-neutral-800">
-              Total Reports Submitted
-            </h3>
-          </div>
-          <p className="text-3xl font-semibold text-neutral-700 mt-2">
-            {data.reports}
-          </p>
+      {/* card 2 */}
+      <div className="w-full p-4 border rounded-lg bg-sidebar">
+        <div className="flex flex-col justify-center items-start gap-y-3">
+          <Button className="pointer-events-none" variant="secondary" size="sm">
+            <Recycle />
+          </Button>
+          <h3 className="text-xl font-medium text-neutral-800">
+            Total Waste Collected
+          </h3>
         </div>
-        {/* card 2 */}
-        <div className="w-full p-4 border rounded-lg bg-sidebar">
-          <div className="flex flex-col justify-center items-start gap-y-3">
-            <Button
-              className="pointer-events-none"
-              variant="secondary"
-              size="sm"
-            >
-              <Recycle />
-            </Button>
-            <h3 className="text-xl font-medium text-neutral-800">
-              Total Waste Collected
-            </h3>
-          </div>
-          <p className="text-3xl font-semibold text-neutral-700 mt-2">
-            {data.collections}
-          </p>
-        </div>
-        {/* card 3 */}
-        <div className="w-full p-4 border rounded-lg bg-sidebar">
-          <div className="flex flex-col justify-center items-start gap-y-3">
-            <Button
-              className="pointer-events-none"
-              variant="secondary"
-              size="sm"
-            >
-              <Medal />
-            </Button>
-            <h3 className="text-xl font-medium text-neutral-800">
-              Leaderboard Rank
-            </h3>
-          </div>
-          <p className="text-3xl font-semibold text-neutral-700 mt-2">
-            {data.rank}
-          </p>
-        </div>
+        <p className="text-3xl font-semibold text-neutral-700 mt-2">
+          {isLoading ? (
+            <Loader className="text-green-500 animate-spin" size={24} />
+          ) : (
+            <span>{data.collections}</span>
+          )}
+        </p>
       </div>
-    );
-  }
+      {/* card 3 */}
+      <div className="w-full p-4 border rounded-lg bg-sidebar">
+        <div className="flex flex-col justify-center items-start gap-y-3">
+          <Button className="pointer-events-none" variant="secondary" size="sm">
+            <Medal />
+          </Button>
+          <h3 className="text-xl font-medium text-neutral-800">
+            Leaderboard Rank
+          </h3>
+        </div>
+        <p className="text-3xl font-semibold text-neutral-700 mt-2">
+          {isLoading ? (
+            <Loader className="text-green-500 animate-spin" size={24} />
+          ) : (
+            <span>{data.rank}</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
 }
