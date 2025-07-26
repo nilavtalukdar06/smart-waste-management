91013b26d60bec603767bc064ae0f85b22493b29
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 12:01:01 +0530

    added main chart component

--- src/components/shared/main-chart.tsx
+++ src/components/shared/main-chart.tsx
@@ -10,17 +10,26 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { AlertCircle, Loader, Recycle } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import Error from "./error";
+import { format, parseISO } from "date-fns";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "../ui/card";
 
 const chartConfig = {
-  desktop: {
-    label: "Reported Waste",
+  reports: {
+    label: "Reported",
     color: "var(--chart-1)",
     icon: AlertCircle,
   },
-  mobile: {
-    label: "Collected Waste",
+  collections: {
+    label: "Collected",
     color: "var(--chart-2)",
     icon: Recycle,
   },
@@ -56,25 +65,60 @@ export default function MainChart() {
   if (data) {
     return (
       <div className="my-6 w-full">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[400px] w-full"
-        >
-          <BarChart accessibilityLayer data={data}>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="_id"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              tickFormatter={(value) => value.slice(5, 10)}
-            />
-            <ChartTooltip content={<ChartTooltipContent />} />
-            <ChartLegend content={<ChartLegendContent />} />
-            <Bar dataKey="reports" fill="var(--color-chart-1)" radius={4} />
-            <Bar dataKey="collections" fill="var(--color-chart-2)" radius={4} />
-          </BarChart>
-        </ChartContainer>
+        <Card className="shadow-none">
+          <CardHeader>
+            <CardTitle>User Analytics</CardTitle>
+            <CardDescription className="font-light">
+              Showing total waste reported and collected by you in the last 7
+              days
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="pl-0">
+            <ChartContainer
+              config={chartConfig}
+              className="aspect-auto h-[400px] w-full"
+            >
+              <BarChart
+                accessibilityLayer
+                data={data}
+                className="ml-[-20px] md:ml-[-10px]"
+              >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="_id"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => format(parseISO(value), "MMM d")}
+                />
+                <YAxis
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  className="ml-0"
+                  allowDecimals={false}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="reports" fill="var(--color-chart-1)" radius={4} />
+                <Bar
+                  dataKey="collections"
+                  fill="var(--color-chart-2)"
+                  radius={4}
+                />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col items-start gap-2 text-sm">
+            <div className="flex gap-2 leading-none font-medium">
+              Your weekly waste activity summary{" "}
+              <Recycle className="h-4 w-4 text-green-500" />
+            </div>
+            <div className="text-muted-foreground leading-snug font-light">
+              Showing total waste reported and collected in the last 7 days
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     );
   }
