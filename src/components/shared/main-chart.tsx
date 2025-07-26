7dac4be6be7732853551e36168ed85433d0b9abd
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 09:17:20 +0530

    added query for fetching charts

--- src/components/shared/main-chart.tsx
+++ src/components/shared/main-chart.tsx
@@ -7,8 +7,11 @@ import {
   ChartLegend,
   ChartLegendContent,
 } from "@/components/ui/chart";
-import { Monitor, Smartphone } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Loader, Monitor, Smartphone } from "lucide-react";
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import Error from "./error";
 
 const chartConfig = {
   desktop: {
@@ -33,24 +36,52 @@ const chartData = [
 ];
 
 export default function MainChart() {
-  return (
-    <div className="my-6 w-full">
-      <ChartContainer config={chartConfig} className="h-[400px] w-full">
-        <BarChart accessibilityLayer data={chartData}>
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="month"
-            tickLine={false}
-            tickMargin={10}
-            axisLine={false}
-            tickFormatter={(value) => value.slice(0, 3)}
-          />
-          <ChartTooltip content={<ChartTooltipContent />} />
-          <ChartLegend content={<ChartLegendContent />} />
-          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-        </BarChart>
-      </ChartContainer>
-    </div>
-  );
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["chart"],
+    queryFn: async () => {
+      const response = await axios.get("/api/analytics/chart");
+      console.log(response.data);
+      return response.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="my-6 w-full flex justify-start items-center gap-x-4">
+        <Loader className="animate-spin text-green-500" size={24} />
+        <p className="text-lg font-medium">Fetching Analytics...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="my-6 max-w-md mr-auto">
+        <Error error="Failed to fetch analytics" />
+      </div>
+    );
+  }
+
+  if (data) {
+    return (
+      <div className="my-6 w-full">
+        <ChartContainer config={chartConfig} className="h-[400px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </div>
+    );
+  }
 }
