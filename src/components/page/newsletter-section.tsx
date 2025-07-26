a9b3b66bdb1df6ec741e1f885f031a8ef16c6b49
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 20:15:05 +0530

    created form schema

--- src/components/page/newsletter-section.tsx
+++ src/components/page/newsletter-section.tsx
@@ -1,8 +1,27 @@
+"use client";
 import { Bell } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Email is not valid" }),
+});
 
 export default function NewsLetterSection() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="w-full py-10 px-5 lg:py-16 mx-auto">
       <div className="max-w-xl text-start sm:text-center mx-auto">
