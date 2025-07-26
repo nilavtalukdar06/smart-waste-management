4c0e8eb4040762e663999d3b6b819fd7e3cbc21d
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 09:34:57 +0530

    added chart data

--- src/components/shared/main-chart.tsx
+++ src/components/shared/main-chart.tsx
@@ -26,15 +26,6 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
-
 export default function MainChart() {
   const { data, isError, isLoading } = useQuery({
     queryKey: ["chart"],
@@ -66,19 +57,19 @@ export default function MainChart() {
     return (
       <div className="my-6 w-full">
         <ChartContainer config={chartConfig} className="h-[400px] w-full">
-          <BarChart accessibilityLayer data={chartData}>
+          <BarChart accessibilityLayer data={data}>
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey="month"
+              dataKey="_id"
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
+              tickFormatter={(value) => value.slice(5, 10)}
             />
             <ChartTooltip content={<ChartTooltipContent />} />
             <ChartLegend content={<ChartLegendContent />} />
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+            <Bar dataKey="reports" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="collections" fill="var(--color-mobile)" radius={4} />
           </BarChart>
         </ChartContainer>
       </div>
