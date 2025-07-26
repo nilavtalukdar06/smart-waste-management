c5db1c7a8780d48c7e11bba0585cf5e643228ff8
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 19:44:23 +0530

    added footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import FeatureSection from "@/components/page/feature-section";
 import HeroSection from "@/components/page/hero-section";
 import NewsLetterSection from "@/components/page/newsletter-section";
 import Navbar from "@/components/shared/navbar";
+import { Copyright } from "lucide-react";
 
 export default function Home() {
   return (
@@ -16,6 +17,12 @@ export default function Home() {
       <FaqSection />
       <CtaSection />
       <NewsLetterSection />
+      <footer className="my-6">
+        <p className="text-neutral-500 text-start sm:text-center flex justify-start sm:justify-center items-center gap-x-4 font-light">
+          <Copyright className="text-green-500 animate-pulse" />{" "}
+          {new Date().getFullYear()} Eco Swachh. All rights reserved.
+        </p>
+      </footer>
     </section>
   );
 }
