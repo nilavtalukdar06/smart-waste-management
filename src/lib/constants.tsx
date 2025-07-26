629c1ce74e33d45703d794cef13d435cee55145a
Nilav <nilavtalukdar06@gmail.com>
2025-07-26 16:02:47 +0530

    added blog items

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,5 +1,5 @@
 import { ICard } from "@/components/shared/card";
-import { Heart, Leaf } from "lucide-react";
+import { CheckCircle, Heart, Leaf, Plus, Recycle } from "lucide-react";
 
 export const cardItems: ICard[] = [
   {
@@ -13,5 +13,29 @@ export const cardItems: ICard[] = [
     title: "Community Support",
     description:
       "You're supporting local communities and waste management workers who keep our neighborhoods clean and healthy.",
+  },
+];
+
+export const blogItems: IBlog[] = [
+  {
+    Icon: Plus,
+    value: "100+",
+    title: "Total Waste Reports",
+    description:
+      "A cumulative count of all waste reports submitted by users across various zones in the last 30 days, reflecting increased public engagement in environmental reporting.",
+  },
+  {
+    Icon: CheckCircle,
+    value: "100+",
+    title: "Waste Collections Done",
+    description:
+      "Represents the number of waste pickups successfully verified by collectors, ensuring proper waste management and cleaner local environments.",
+  },
+  {
+    Icon: Recycle,
+    value: "34%",
+    title: "Recycling Rate",
+    description:
+      "Indicates the proportion of total waste that has been identified as recyclable and processed through appropriate channels, showcasing our progress towards sustainability.",
   },
 ];
